bd2d37c1466e9cb50804ffba28c276b402d2a91b
NiroLevi133 <nirlevi1998@gmail.com>
2025-10-20 22:10:00 +0300

    Delete New_v1 directory

--- New_v1/test_api.py
+++
@@ -1,9 +0,0 @@
-from logic import load_excel, top_matches
-
-import pandas as pd
-
-# טען את אנשי הקשר
-contacts_df = load_excel("contacts_file.xlsx")  # תשנה לשם הקובץ האמיתי שלך
-
-# בדוק מוזמן ספציפי
-print(top_matches("ניר לוי", contacts_df))
